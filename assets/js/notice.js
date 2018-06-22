d477924d2d868d38eaa0192e9c23501aa3961790
fizminho <40485506+fizminho@users.noreply.github.com>
2018-06-22 15:54:28 +0800

    lol

---
+++ assets/js/notice.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Rick Stewart on Nov/14/2016.
+ */
+
+
+(function main() {
+    'use strict';
+    $('#lynda-link').mouseover(function () {
+        this.style.color = 'yellow';
+    });
+
+    $('#lynda-link').mouseout(function () {
+        this.style.color = '#30096A';
+    });
+
+    $('#lynda-link').click(function () {
+        window.open('https://www.lynda.com/React-js-tutorials/Learn-React-js-Basics/519668-2.html');
+    });
+})();
